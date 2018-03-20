1060faef54013f024e54411bc9568d100dd554f7
JieanYang <yja8534786@gmail.com>
2018-03-20 10:17:54 +0100

    images;

--- Site Web/index.php
+++ Site Web/index.php
@@ -10,10 +10,15 @@
 </head>
 <body>
     <div class="container-fluid red col-xs-4 col-sm-12 col-md-12 col-lg-12 " >
-            <img src="悬崖上的金鱼姬.jpg" style="position: absolute;left:380px;top: 20px" width="550px">  
-        <div style="border-color: black" width="50px">
+            <img src="finance.jpg" style="position: absolute;left:380px;top: 20px" width="550px">  
+       <!--  <div style="border-color: black" width="50px">
             <h4> Mon portefeuille </h4>
-        </div>
+        </div> -->
+
+        <div class="dim">
+            <h6>  &nbsp &nbsp<strong>Mon portefeuille</strongs> </h6>
+            <img src="cash.png" style="position: absolute;left:20px;top: 110px" width="60px">
+       </div>
 
         <div style="position: absolute;left: 450px;top: 470px">
             <a class="btn btn-info" href="image_stock.php?Korian">Consulter Korian</a>
@@ -34,7 +39,7 @@
             <a class="btn btn-info" href="image_stock.php?L_oreal">Consulter Loreal</a>
         </div>        
         <div >
-            <img src="悬崖上的金鱼姬.jpg" style="position: absolute;top: 450px;left: 600px">
+            <img src="liste.png" style="position: absolute;top: 450px;left: 650px">
         </div>
     </div>        
 </body>
